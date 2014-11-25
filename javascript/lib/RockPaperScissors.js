b60cb886c864a8c8857579658d93dddda1237ddd
imarkwick <izzymarkwick@gmail.com>
2014-11-25 16:05:19 +0000

    all tests all green

--- javascript/lib/RockPaperScissors.js
+++ javascript/lib/RockPaperScissors.js
@@ -6,12 +6,28 @@ Player.prototype.picks = function(pick) {
 	this.pick = pick;
 };
 
+
+
 function Game(player1, player2) {
 	this.player1 = player1;
 	this.player2 = player2;
 };
 
 Game.prototype.winner = function() {
-	return this.player1;
+	if(this.player1.pick === this.player2.pick) {return null}
+	if(this.PAIRS[this.player1.pick]['beats'] === this.player2.pick){
+		return this.player1;
+	}
+	else {
+		return this.player2;
+	}
 };
+
+Game.prototype.PAIRS = {
+	 	rock:  { beats: "scissors" }, 
+	 	paper: { beats: "rock"}, 
+	 	scissors: { beats: "paper"} 
+};
+
+
 
